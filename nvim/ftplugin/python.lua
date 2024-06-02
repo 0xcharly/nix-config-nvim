ccd65fba2a66edfd6c3db8088f17de8072c7652e
Charly Delay <charly@delay.gg>
2024-06-02 22:08:45 +0900

    Add python-lsp-server config

---
+++ nvim/ftplugin/python.lua
@@ -0,0 +1,25 @@
+local pylsp_cmd = 'pylsp'
+
+if vim.fn.executable(pylsp_cmd) ~= 1 then
+  return
+end
+
+local lsp = require('user.lsp')
+
+---@diagnostic disable-next-line: missing-fields
+vim.lsp.start {
+  name = 'pylsp',
+  cmd = { nixd_cmd, '--inlay-hints', '--semantic-tokens' },
+  root_dir = vim.fs.dirname(vim.fs.find({ 'flake.nix', '.git' }, { upward = true })[1]),
+  on_attach = lsp.on_attach,
+  capabilities = lsp.make_client_capabilities(),
+  settings = {
+    pylsp = {
+      plugins = {
+        black = {
+          enabled = true -- Disables autopep8 and yapf.
+        },
+      }
+    }
+  },
+}
