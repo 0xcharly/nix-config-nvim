45b0dd55728cc2199966934a990d4bbca3fb744f
Charly Delay <charly@delay.gg>
2024-06-02 22:11:12 +0900

    Fix python-lsp-server command

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -9,7 +9,7 @@ local lsp = require('user.lsp')
 ---@diagnostic disable-next-line: missing-fields
 vim.lsp.start {
   name = 'pylsp',
-  cmd = { nixd_cmd, '--inlay-hints', '--semantic-tokens' },
+  cmd = { nixd_cmd },
   root_dir = vim.fs.dirname(vim.fs.find({ 'flake.nix', '.git' }, { upward = true })[1]),
   on_attach = lsp.on_attach,
   capabilities = lsp.make_client_capabilities(),
