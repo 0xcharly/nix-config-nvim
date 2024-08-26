e9e0f09b4750a60a3e31d47d95961b737e6b4d35
Charly Delay <0@0xcharly.com>
2024-08-26 16:44:15 +0900

    Tentative config for `rust-analyzer` LSP

--- nvim-config/plugin/lspconfig-rust.lua
+++ nvim-config/plugin/lspconfig-rust.lua
@@ -11,4 +11,21 @@ lspconfig.rust_analyzer.setup {
   cmd = { rust_analyzer_cmd },
   on_attach = lsp.on_attach,
   capabilities = lsp.make_client_capabilities(),
+  settings = {
+    ['rust-analyzer'] = {
+      cargo = {
+        allFeatures = true,
+        loadOutDirsFromCheck = true,
+        runBuildScripts = true,
+      },
+      procMacro = {
+        enable = true,
+        ignored = {
+          ['async-trait'] = { 'async_trait' },
+          ['napi-derive'] = { 'napi' },
+          ['async-recursion'] = { 'async_recursion' },
+        },
+      },
+    },
+  },
 }
