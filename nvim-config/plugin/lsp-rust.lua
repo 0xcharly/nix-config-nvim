c7af91b57bd744f8bc1cd064bb24996b73542e55
Charly Delay <0@0xcharly.com>
2024-08-26 15:33:47 +0900

    `vim.g.rustaceanvim` should be a table, not a function (?)

--- nvim-config/plugin/lsp-rust.lua
+++ nvim-config/plugin/lsp-rust.lua
@@ -1,30 +1,25 @@
 local lsp = require('user.lsp')
 
----@return RustaceanOpts
-vim.g.rustaceanvim = function()
-  ---@type RustaceanOpts
-  local rustacean_opts = {
-    server = {
-      on_attach = lsp.on_attach,
-      capabilities = lsp.make_client_capabilities(),
-      default_settings = {
-        ['rust-analyzer'] = {
-          cargo = {
-            allFeatures = true,
-            loadOutDirsFromCheck = true,
-            runBuildScripts = true,
-          },
-          procMacro = {
-            enable = true,
-            ignored = {
-              ['async-trait'] = { 'async_trait' },
-              ['napi-derive'] = { 'napi' },
-              ['async-recursion'] = { 'async_recursion' },
-            },
+vim.g.rustaceanvim = {
+  server = {
+    on_attach = lsp.on_attach,
+    capabilities = lsp.make_client_capabilities(),
+    default_settings = {
+      ['rust-analyzer'] = {
+        cargo = {
+          allFeatures = true,
+          loadOutDirsFromCheck = true,
+          runBuildScripts = true,
+        },
+        procMacro = {
+          enable = true,
+          ignored = {
+            ['async-trait'] = { 'async_trait' },
+            ['napi-derive'] = { 'napi' },
+            ['async-recursion'] = { 'async_recursion' },
           },
         },
       },
     },
-  }
-  return rustacean_opts
-end
+  },
+}
