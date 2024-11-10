206a836d08ad54f12d896ebf7bef33fc310417d0
Charly Delay <0@0xcharly.com>
2024-11-10 17:21:15 +0900

    Some refactoring of the cmp config

--- nvim-config/plugin/completion.lua
+++ nvim-config/plugin/completion.lua
@@ -1,32 +1,32 @@
 local cmp = require('cmp')
 
--- local cmp_mapping_next_item =
---   cmp.mapping(cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }, { 'i', 'c' })
--- local cmp_mapping_prev_item =
---   cmp.mapping(cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }, { 'i', 'c' })
--- local cmp_mapping_confirm = cmp.mapping(
---   cmp.mapping.confirm {
---     behavior = cmp.ConfirmBehavior.Insert,
---     select = true,
---   },
---   { 'i', 'c' }
--- )
-local cmp_mapping_next_item = cmp.mapping.select_next_item()
-local cmp_mapping_prev_item = cmp.mapping.select_prev_item()
-local cmp_mapping_confirm = cmp.mapping.confirm { select = true }
+-- Sorts symbols that start by an underscore last.
+-- Copied from cmp-under-comparator; don't need a plugin for this.
+-- https://github.com/lukas-reineke/cmp-under-comparator
+local function cmp_config_compare_underscore(lhs, rhs)
+  local _, lhs_under = lhs.completion_item.label:find('^_+')
+  local _, rhs_under = rhs.completion_item.label:find('^_+')
+  lhs_under = lhs_under or 0
+  rhs_under = rhs_under or 0
+  if lhs_under > rhs_under then
+    return false
+  elseif lhs_under < rhs_under then
+    return true
+  end
+end
 
 cmp.setup {
   mapping = cmp.mapping.preset.insert {
-    ['<C-j>'] = cmp_mapping_next_item,
-    ['<C-k>'] = cmp_mapping_prev_item,
-    ['<C-S-j>'] = cmp_mapping_next_item,
-    ['<C-S-k>'] = cmp_mapping_prev_item,
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<C-S-j>'] = cmp.mapping.select_next_item(),
+    ['<C-S-k>'] = cmp.mapping.select_prev_item(),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-a>'] = cmp.mapping.abort(),
-    ['<C-y>'] = cmp_mapping_confirm,
-    ['<C-S-y>'] = cmp_mapping_confirm,
-    ['<CR>'] = cmp_mapping_confirm,
+    ['<C-y>'] = cmp.mapping.confirm { select = true },
+    ['<C-S-y>'] = cmp.mapping.confirm { select = true },
+    ['<CR>'] = cmp.mapping.confirm { select = true },
     ['<c-space>'] = cmp.mapping.complete {},
     ['<C-q>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
     ['<Tab>'] = cmp.config.disable,
@@ -42,26 +42,14 @@ cmp.setup {
       cmp.config.compare.offset,
       cmp.config.compare.exact,
       cmp.config.compare.score,
-
-      -- Copied from cmp-under; don't need a plugin for this.
-      function(entry1, entry2)
-        local _, entry1_under = entry1.completion_item.label:find('^_+')
-        local _, entry2_under = entry2.completion_item.label:find('^_+')
-        entry1_under = entry1_under or 0
-        entry2_under = entry2_under or 0
-        if entry1_under > entry2_under then
-          return false
-        elseif entry1_under < entry2_under then
-          return true
-        end
-      end,
-
+      cmp_config_compare_underscore,
       cmp.config.compare.kind,
       cmp.config.compare.sort_text,
       cmp.config.compare.length,
       cmp.config.compare.order,
     },
   },
+  completion = { completeopt = 'menu,menuone,noinsert' },
   window = {
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
