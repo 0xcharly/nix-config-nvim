2ed386354a1baf25de379d9894780a703cb6067b
Charly Delay <0@0xcharly.com>
2025-05-31 20:14:52 +0900

    Remove `nvim-override` overlay as it was triggering many traces:
    ```
    trace: using non-memoized system i686-linux
    trace: using non-memoized system i686-linux
    trace: using non-memoized system i686-linux
    trace: using non-memoized system i686-linux
    trace: using non-memoized system i686-linux
    trace: using non-memoized system i686-linux
    trace: using non-memoized system i686-linux
    trace: using non-memoized system i686-linux
    trace: using non-memoized system wasm32-wasi
    ```

--- overlays.nix
+++ overlays.nix
@@ -1,20 +1,8 @@
-{
-  inputs,
-  withSystem,
-  ...
-}: {
+{inputs, ...}: {
   imports = [inputs.flake-parts.flakeModules.easyOverlay];
 
   # Injects all packages declared by this flake.
   perSystem = {config, ...}: {
     overlayAttrs.nix-config-nvim = config.packages;
   };
-
-  # Overrides `pkgs.nvim` package with the distribution declared by this flake.
-  flake.overlays.nvim-override = _final: prev:
-    withSystem prev.stdenv.hostPlatform.system (
-      {config, ...}: {
-        nvim = config.packages.default;
-      }
-    );
 }
