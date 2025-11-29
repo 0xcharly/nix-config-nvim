cc5d15cc5ec24f2b8ed2f327271650022d8ec557
Charly Delay <charly@delay.gg>
2025-11-29 17:34:42 +0900

    Remove deprecated attribute

--- mk-nvim-dist.nix
+++ mk-nvim-dist.nix
@@ -114,8 +114,6 @@
 
     neovimConfig = neovimUtils.makeNeovimConfig {
       inherit customLuaRC;
-      # TODO(25.11): Remove deprecated `luaRcContent`.
-      luaRcContent = customLuaRC;
       wrapRc = true;
 
       viAlias = false;
