02adaead25753842456b8a06f44cab23f4e48d4d
Charly Delay <charly@delay.gg>
2025-11-29 21:42:27 +0900

    Simplify inputs

--- flake.nix
+++ flake.nix
@@ -1,45 +1,41 @@
 {
   description = "Neovim config";
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["aarch64-darwin" "aarch64-linux" "x86_64-linux"];
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-      perSystem = {pkgs, ...}: let
-        mkNvimDist = pkgs.callPackage ./mk-nvim-dist.nix {};
-        nvimConfig = {
-          src = ./nvim-config;
-          runtime = [./nvim-runtime];
-          patches = [];
-          plugins = pkgs.callPackage ./nvim-plugins.nix {};
-        };
-      in {
-        packages = rec {
-          # The default package that bundles the entire neoviw configuration
-          # without any dependency.
-          default = mkNvimDist nvimConfig;
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = fn:
+      nixpkgs.lib.genAttrs nixpkgs.lib.platforms.linux (
+        system: fn nixpkgs.legacyPackages.${system}
+      );
+  in {
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
 
-          # Augment the default package with more plugins.
-          withExtraPlugins = plugins:
-            default.override (prev: {plugins = prev.plugins ++ plugins;});
-
-          # A custom build with an extra lush-nvim plugin to work on the theme.
-          debug-colorscheme = withExtraPlugins [pkgs.vimPlugins.lush-nvim];
-
-          # A custom debug build without any configuration or plugins.
-          debug-norc = default.override {
-            src = ./.;
-            plugins = [];
-          };
-        };
+    packages = forAllSystems (pkgs: let
+      mkNvimDist = pkgs.callPackage ./mk-nvim-dist.nix {};
+      nvimConfig = {
+        src = ./nvim-config;
+        runtime = [./nvim-runtime];
+        patches = [];
+        plugins = pkgs.callPackage ./nvim-plugins.nix {};
       };
-    };
+    in rec {
+      # The default package that bundles the entire neoviw configuration
+      # without any dependency.
+      default = mkNvimDist nvimConfig;
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
+      # Augment the default package with more plugins.
+      withExtraPlugins = plugins:
+        default.override (prev: {plugins = prev.plugins ++ plugins;});
+
+      # A custom build with an extra lush-nvim plugin to work on the theme.
+      debug-colorscheme = withExtraPlugins [pkgs.vimPlugins.lush-nvim];
+
+      # A custom debug build without any configuration or plugins.
+      debug-norc = default.override {
+        src = ./.;
+        plugins = [];
+      };
+    });
   };
 }
