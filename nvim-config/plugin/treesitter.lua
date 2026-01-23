59f6c12c5ea2ef35e52343847f9144d7de44ecc3
Charly Delay <delay@google.com>
2026-01-23 18:02:15 +0900

    Update inputs

--- nvim-config/plugin/treesitter.lua
+++
@@ -1,6 +0,0 @@
----@diagnostic disable: missing-fields
-require('nvim-treesitter.configs').setup {
-  highlight = { enable = true },
-  indent = { enable = true },
-  autopairs = { enable = true },
-}
